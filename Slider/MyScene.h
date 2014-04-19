bde720126176ee123c040bd9fb949a15913cf27b
Grant Spence <gcs278@vt.edu>
2014-04-18 22:47:17 -0400

    Initial Commit

---
+++ Slider/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  Slider
+//
+
+//  Copyright (c) 2014 Grant Spence. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ Slider/ViewController.m
@@ -0,0 +1,51 @@
+//
+//  ViewController.m
+//  Slider
+//
+//  Created by Grant Spence on 4/18/14.
+//  Copyright (c) 2014 Grant Spence. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "MyScene.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Configure the view.
+    SKView * skView = (SKView *)self.view;
+    skView.showsFPS = YES;
+    skView.showsNodeCount = YES;
+    
+    // Create and configure the scene.
+    SKScene * scene = [MyScene sceneWithSize:skView.bounds.size];
+    scene.scaleMode = SKSceneScaleModeAspectFill;
+    
+    // Present the scene.
+    [skView presentScene:scene];
+}
+
+- (BOOL)shouldAutorotate
+{
+    return YES;
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return UIInterfaceOrientationMaskAllButUpsideDown;
+    } else {
+        return UIInterfaceOrientationMaskAll;
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+@end
